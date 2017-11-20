b7bc41f7bda4820e7952435d0372eaa57bf73a65
ArtCouSan <artcousan@gmail.com>
2017-11-20 16:12:11 -0200

    Inicio

---
+++ app/src/main/java/com/example/casa/mobile/Mobile.kt
@@ -0,0 +1,12 @@
+package com.example.casa.mobile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Mobile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobile)
+    }
+}
